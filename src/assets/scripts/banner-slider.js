7b6ecf5f7d7ec0ba09a617aec1003208783c2da6
AlexanderMezhenskyi <alexmezhenskyi@gmail.com>
2017-06-28 23:16:53 +0300

    added sections of banner and team

---
+++ src/assets/scripts/banner-slider.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$('.ma-slider').slick({
+		slidesToShow:1,
+		dots: false,
+		arrows: true
+	});
+});
---
+++ src/assets/scripts/menu-scroll.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$(".ma-menu").sticky({topSpacing:0, center:true});
+});
